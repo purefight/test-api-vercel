b982f2e6ed12190d0ceaf35416b0d65045820fcc
purefight <peranat.c@kkumail.com>
2024-01-04 01:17:22 +0700

    pp

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from os import getenv
 
 if __name__ == "__main__":
     port = int(getenv("PORT",8000))
-    uvicorn.run("app.app:app",host = "0.0.0.0", port=port, reload=True)
+    uvicorn.run("app.api:app",host = "0.0.0.0", port=port, reload=True)
